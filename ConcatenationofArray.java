d70d82516b546ae45353a99a4b03df771aa3e15a
Abdul Harish Khan <khan.abdulharish@outlook.com>
2026-03-23 09:35:12 +0530

    day 4 commit

---
+++ ConcatenationofArray.java
@@ -0,0 +1,22 @@
+public class ConcatenationofArray {
+
+        public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+
+        int ans[] = new int[2 * n];
+
+        for(int i=0;i<n;i++){
+             ans[i] = nums[i] ;
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int [] nums = {1,2,1};
+        ConcatenationofArray concatenationofArray = new ConcatenationofArray();
+        int[] result = concatenationofArray.getConcatenation(nums);
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i] + " ");
+        }
+    }
+}
