5ef0f6ead2972a0963f8a651e651e53834c9410e
Abdul Harish Khan <khan.abdulharish@outlook.com>
2026-03-27 23:52:31 +0530

    day 8 : Add SQL solution for identifying invalid tweets based on content length

---
+++ SQL 50 LEETCODE/Lc1683InvalidTweets.sql
@@ -0,0 +1,27 @@
+-- 1683. Invalid Tweets
+
+
+
+-- Table: Tweets
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+-- This table contains all the tweets in a social media app.
+ 
+
+-- Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- Return the result table in any order.
+
+-- Solutions:
+
+# Write your MySQL query statement below
+select tweet_id from Tweets
+where CHAR_LENGTH(content) > 15;
+
